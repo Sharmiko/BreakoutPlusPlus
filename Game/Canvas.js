a5d659304ec8ead4f6056e6a6c797352f724ba82
Giorgi Sharmiashvili <sharmiko123@gmail.com>
2020-05-21 14:28:41 +0400

    [refactor] update Canvas

--- Game/Canvas.js
+++ Game/Canvas.js
@@ -1,46 +1,25 @@
+// Select canvas from HTML and get its context
+var canvas = document.querySelector("#canvas");
+var ctx = canvas.getContext("2d");
+
 /*
-*
-*   Author: Giorgi Sharmiashvili
-*
-*   Project: Breakout++
-*
-*   Description: Advanced Version of arcanoid/breakout/break breaker
-*                game with more features and improvements. This game
-*                is being improved from time to time, so this is not
-*                the last working version of it.
-*   
+*   Canvas Object that contains
+*   widht and height of the canvas
+*   window and controls canvas
+*   animations.
 */
-
-
-class Canvas 
-{
-    constructor() 
-    {
-        console.log("Canvas init");
-        // Select canvas from HTML and get its context
-        this.canvas = document.querySelector("#canvas");
-        this.ctx = canvas.getContext("2d");
-        
-        /*
-        *   Canvas Object that contains
-        *   widht and height of the canvas
-        *   window and controls canvas
-        *   animations.
-        */
-        this.properties = {
-            width: this.canvas.width,
-            height: this.canvas.height,
-            animation: null,
-            menuClicked: false,
-            pause: false,
-            isGameStarted: false,
-            restartGame: false,
-            askForRestart: false,
-            playClicked: false,
-            levelBricks: [],
-            text: "",
-            textColor: "",
-            terminateLevelHover: false
-        };
-    }
+const Canvas = {
+    width: canvas.width,
+    height: canvas.height,
+    animation: null,
+    menuClicked: false,
+    pause: false,
+    isGameStarted: false,
+    restartGame: false,
+    askForRestart: false,
+    playClicked: false,
+    levelBricks: [],
+    text: "",
+    textColor: "",
+    terminateLevelHover: false
 }
