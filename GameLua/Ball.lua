51816f9bba4a07da009c9a4c776fd63510d8ec95
Sharmiko <sharmiko123@gmail.com>
2020-09-04 17:49:11 +0400

    add comments

--- GameLua/Ball.lua
+++ GameLua/Ball.lua
@@ -1,6 +1,8 @@
 Ball = Object:extend()
 
+--[[ Ball constructor
 
+--]]
 function Ball:new()
     Ball.super.new()
     self.x = love.graphics.getWidth() / 2
@@ -11,13 +13,20 @@ function Ball:new()
 end 
 
 
+--[[ Function that drwas a ball
+
+--]]
 function Ball:draw()
     love.graphics.setColor(65/255, 179/255, 163/255)
     love.graphics.circle("fill", self.x, self.y, self.radius)
     love.graphics.setColor(1, 1, 1)
+    print(self.radius)
 end 
 
 
+--[[ Function that updates ball position
+     and checks for collisions
+--]]
 function Ball:update(dt, paddle)
     self:ballPaddleCollision(paddle)
 
@@ -38,6 +47,9 @@ function Ball:update(dt, paddle)
 end 
 
 
+--[[ Function that checks Ball and paddle
+     collision
+--]]
 function Ball:ballPaddleCollision(paddle)
     if (self.x + self.radius / 2 > (paddle.x - paddle.width / 2)) and 
        (self.x - self.radius / 2< (paddle.x + paddle.width / 2)) and 
