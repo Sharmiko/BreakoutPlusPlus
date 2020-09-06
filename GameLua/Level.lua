d585657215f62cc4432067c82a795cad715355dd
Sharmiko <sharmiko123@gmail.com>
2020-09-06 22:09:17 +0400

    small update

--- GameLua/Level.lua
+++ GameLua/Level.lua
@@ -12,22 +12,23 @@ function Level:new()
         startPaddingY = 15,
         array = {
             level_1 = {
-                {1, 1, 1, 0, 0, 1, 1, 1},
-                {1, 1, 1, 0, 0, 1, 1, 1},
-                {1, 1, 1, 0, 0, 1, 1, 1},
-                {1, 1, 1, 0, 0, 1, 1, 1},
-                {1, 1, 1, 0, 0, 1, 1, 1},
-                {1, 1, 1, 0, 0, 1, 1, 1},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1}
+                arr = {
+                    {1, 1, 1, 0, 0, 1, 1, 1},
+                    {1, 1, 1, 0, 0, 1, 1, 1},
+                    {1, 1, 1, 0, 0, 1, 1, 1},
+                    {1, 1, 1, 0, 0, 1, 1, 1},
+                    {1, 1, 1, 0, 0, 1, 1, 1},
+                    {1, 1, 1, 0, 0, 1, 1, 1},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1}
+                },
+                text = "Level 1",
+                stars = 3,
+                taken = 0
             },
-            x = 0,
-            y = 0,
-            text = "Level 1",
-            stars = 3,
-            taken = 0
+
         },
         buttonPositions = {},
         hoverPadding = 10
@@ -45,21 +46,29 @@ function Level:new()
 end 
 
 
+function Level:draw()
+    self:drawLevelButtons()
+end 
+
 function Level:drawLevelButtons()
+    local index = 1
     for key in pairs(self.levels["array"])
     do
         local level = self.levels["array"][key]
-        self:drawLevelButton(level["x"])
+        self:drawLevelButton(self.levels["buttonPositions"][1][1], self.levels["buttonPositions"][1][2], 
+            self.levels["width"], self.levels["height"], "Level 1", 24, 0)
+        index = index + 1
     end
 end 
 
 
 function Level:drawLevelButton(x, y, width, height, text, fontSize, padding)
     love.graphics.setColor(0, 0, 0)
-    love.graphics.rectangle(x - padding, y - padding, width + 2 * padding, height + 2 * padding)
+    love.graphics.rectangle("line", x - padding, y - padding, width + 2 * padding, height + 2 * padding)
     font = love.graphics.newFont(fontSize)
-    textWidth = font:getWidth()
-    love.graphics.print(text, x + width / 2 - textWidth / 2, y + height / 4)
+    textWidth = font:getWidth(text)
+    love.graphics.print(text, x + width / 2 - textWidth / 2 , y + height / 4)
+
 end 
 
 function Level:numLevels()
--- GameLua/main.lua
+++ GameLua/main.lua
@@ -1,21 +1,53 @@
 function love.load()
     Object = require "classic"
+
     require "Paddle"
     require "Ball"
+    require "Menu"
+    require "Level"
 
     paddle = Paddle()
     ball = Ball()
+    menu = Menu()
+    level = Level()
+
+    state = {
+        playButtonClicked =  false,
+        optionsButtonClicked =  false,
+        aboutButtonClicked = false,
+        levelButtonClicked = false
+    }
+
 end 
 
 
 function love.update(dt)
-    paddle:update(dt)
-    ball:update(dt, paddle)
+    --paddle:update(dt)
+    --ball:update(dt, paddle)
+    if (state["playButtonClicked"])
+    then
+        love.mouse.setCursor()
+        --state["playButtonClicked"] = false
+    elseif(state["optionsButtonClicked"])
+    then
+        state["optionsButtonClicked"] = false
+    elseif(state["aboutButtonClicked"])
+    then
+        state["aboutButtonClicked"] = false 
+    else
+        menu:update(state)
+    end
 end 
 
 
 function love.draw()
     love.graphics.setBackgroundColor(133/255, 205/255, 202/255, 0)
-    paddle:draw()
-    ball:draw()
+    if (state["playButtonClicked"])
+    then 
+        level:draw()
+    else
+        menu:draw()
+    end 
+    --paddle:draw()
+    --ball:draw()
 end 
