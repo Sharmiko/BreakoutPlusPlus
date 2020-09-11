10e7916124e6029a851ec304dc5941541c2e7ef8
Sharmiko <sharmiko123@gmail.com>
2020-09-11 12:24:45 +0400

    add color change on collision

--- Tests/CollisionTests.lua
+++ Tests/CollisionTests.lua
@@ -14,18 +14,20 @@ function CollisionTests:new()
         x = self.width / 2 - 100,
         y = self.height / 2,
         width = 200,
-        height = 70
+        height = 70,
+        color = {226, 125, 96}
     }
 end 
 
 function CollisionTests:update()
-
+    self:updateBall()
+    self:ballRectangleCollision()
 end 
 
 
 function CollisionTests:draw()
-    self:drawBall()
     self:drawRectangle()
+    self:drawBall()
 end 
 
 
@@ -36,8 +38,30 @@ function CollisionTests:drawBall()
 end 
 
 
+function CollisionTests:updateBall()
+    local mouseX = love.mouse.getX()
+    local mouseY = love.mouse.getY()
+    self.ball.x = mouseX 
+    self.ball.y = mouseY
+end 
+
+
+function CollisionTests:ballRectangleCollision()
+    if (self.ball.x + self.ball.radius / 2 > (self.rectangle.x - self.rectangle.width / 2)) and 
+       (self.ball.x - self.ball.radius / 2< (self.rectangle.x + self.rectangle.width / 2)) and 
+       (self.ball.y + self.ball.radius / 2> (self.rectangle.y - self.rectangle.height / 2)) and  
+       (self.ball.y - self.ball.radius / 2< (self.rectangle.y + self.rectangle.height / 2)) 
+    then 
+        self.rectangle.color = {235, 64, 52}
+    else
+        self.rectangle.color = {226, 125, 96}
+    end 
+end 
+
+
 function CollisionTests:drawRectangle()
-    love.graphics.setColor(226/255, 125/255, 96/255)
+    local color = self.rectangle.color
+    love.graphics.setColor(color[1] / 255, color[2] / 255, color[3] / 255)
     love.graphics.rectangle("fill", self.rectangle.x, self.rectangle.y, self.rectangle.width, self.rectangle.height)
     love.graphics.setColor(1, 1, 1)
 end 
