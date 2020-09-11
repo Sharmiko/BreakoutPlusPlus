716bb2183d5a6e0d75259eae0718f63ca58c8cf0
Sharmiko <sharmiko123@gmail.com>
2020-09-12 00:34:00 +0400

    add rectangle wall collision detection tests

--- Tests/CollisionTests.lua
+++ Tests/CollisionTests.lua
@@ -7,39 +7,54 @@ function CollisionTests:new()
     self.ball = {
         x = self.width / 2,
         y = self.height / 2 - 200,
-        radius = 20
+        radius = 20,
+        color = {65, 179, 163}
     }
 
     self.rectangle = {
         x = self.width / 2 - 100,
-        y = self.height / 2,
+        y = self.height / 2 - 200,
         width = 200,
         height = 70,
-        color = {226, 125, 96}
+        color = {146, 7, 232}
+    }
+
+    self.square = {
+        x = self.width / 2 - 100,
+        y = self.height / 2,
+        width = 200,
+        height = 200,
+        color = {245, 179, 66}
     }
 end 
 
 function CollisionTests:update()
     self:updateBall()
+    self:ballSquareWallCollision(self.ball.x, self.ball.y, self.ball.radius, 
+        self.square.x, self.square.y, self.square.width, self.square.height)
     local coll = self:ballRectangleCollision(self.ball.x, self.ball.y, self.ball.radius,
         self.rectangle.x, self.rectangle.y, self.rectangle.width, self.rectangle.height)
     if coll
     then
         self.rectangle.color = {235, 64, 52}
+        self.ball.color = {219, 0, 0}
     else
-        self.rectangle.color = {226, 125, 96}
+        self.rectangle.color = {146, 7, 232}
+        self.ball.color = {226, 125, 96}
     end 
 end 
 
 
 function CollisionTests:draw()
     self:drawRectangle()
+    self:drawSquare()
     self:drawBall()
 end 
 
 
 function CollisionTests:drawBall()
-    love.graphics.setColor(65/255, 179/255, 163/255)
+    local color = self.ball.color 
+    love.graphics.setColor(color[1] / 255, color[2] / 255, color[3] / 255)
     love.graphics.circle("fill", self.ball.x, self.ball.y, self.ball.radius)
     love.graphics.setColor(1, 1, 1)
 end 
@@ -75,4 +90,63 @@ function CollisionTests:drawRectangle()
     love.graphics.setColor(color[1] / 255, color[2] / 255, color[3] / 255)
     love.graphics.rectangle("fill", self.rectangle.x, self.rectangle.y, self.rectangle.width, self.rectangle.height)
     love.graphics.setColor(1, 1, 1)
+end 
+
+
+function CollisionTests:drawSquare()
+    local color = self.square.color 
+    love.graphics.setColor(color[1] / 255, color[2] / 255, color[3] / 255)
+    love.graphics.rectangle("fill", self.square.x, self.square.y, self.square.width, self.square.height)
+    love.graphics.setColor(1, 1, 1)
+end 
+
+
+function CollisionTests:ballSquareWallCollision(ballX, ballY, ballRadius, squareX, squareY, squareWidth, squareHeight)
+    local leftRect = {
+        x = squareX,
+        y = squareY + 10,
+        width = 2,
+        height = squareHeight - 10
+    }
+
+    local upperRect = {
+        x = squareX + 10,
+        y = squareY,
+        width = squareWidth - 10,
+        height = 10
+    }
+
+    local rightRect = {
+        x = squareX + squareWidth - 10,
+        y = squareY + 10,
+        width = 2,
+        height = squareHeight - 10
+    }
+
+    local lowerRect = {
+        x = squareX + 10,
+        y = squareY + squareHeight - 10,
+        width = squareWidth,
+        height = 10
+    }
+
+    if (self:ballRectangleCollision(ballX, ballY, ballRadius, leftRect.x, leftRect.y, leftRect.width, leftRect.height))
+    then
+        print("LEFT")
+        self.square.color = {3, 82, 252} 
+    elseif (self:ballRectangleCollision(ballX, ballY, ballRadius, upperRect.x, upperRect.y, upperRect.width, upperRect.height))
+    then
+        print("UPPER")
+        self.square.color = {3, 252, 248} 
+    elseif (self:ballRectangleCollision(ballX, ballY, ballRadius, rightRect.x, rightRect.y, rightRect.width, rightRect.height))
+    then 
+        print("RIGHT")
+        self.square.color = {252, 198, 3}
+    elseif (self:ballRectangleCollision(ballX, ballY, ballRadius, lowerRect.x, lowerRect.y, lowerRect.width, lowerRect.height))
+    then
+        print("LOWER")
+        self.square.color = {252, 3, 252} 
+    else 
+        self.square.color = {245, 179, 66}
+    end 
 end 
