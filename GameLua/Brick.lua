c0abe08287a6a82ffcf864e63ec4527731b73055
Giorgi Sharmiashvili <sharmiko123@gmail.com>
2020-09-22 16:00:12 +0400

    add outline to bricks

--- GameLua/Brick.lua
+++ GameLua/Brick.lua
@@ -20,6 +20,7 @@ function Brick:new(x, y, width, height, hits)
     self.colors = {}
     self.colors[0] = {232, 168, 124}
     self.colors[1] = {195, 141, 158}
+    self.lineWidth = 1
 end 
 
 
@@ -30,8 +31,12 @@ function Brick:draw()
     if (self.hits > 0)
     then 
         local color = self.colors[self.hits - 1]
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.setLineWidth(self.lineWidth)
+        love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
         love.graphics.setColor(color[1] / 255, color[2] / 255, color[3] / 255)
-        love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+        love.graphics.rectangle("fill", self.x + self.lineWidth, self.y, 
+            self.width - self.lineWidth, self.height - self.lineWidth)
         love.graphics.setColor(1, 1, 1)
     end 
 end 
